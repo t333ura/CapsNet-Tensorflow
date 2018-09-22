7381c948b7e7b7b22114cc2ba23f0dccecd5d50d
t333ura <t333ura@gmail.com>
2018-09-23 05:16:50 +0900

    backup

--- dockerrun.bat
+++
@@ -1,2 +0,0 @@
-docker run -it --name "capsnet" -v c:/Users/Tatsuki/projects/CapsNet-Tensorflow:/workdir -w /workdir -p 6006:6006 tensorflow/tensorflow:latest-py3 bash
-pause
--- dockerrun_jupyter.bat
+++
@@ -1,2 +0,0 @@
-docker run -it --name "capsnet_jupyter" -v c:/Users/Tatsuki/projects/capsule_networks:/notebooks/workdir -w /notebooks -p 8888:8888 tensorflow/tensorflow:latest-py3
-pause
